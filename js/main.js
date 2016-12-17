cdc123c834a57fc0974475cae07f96689495fc94
ArnasJ <hettfield@gmail.com>
2016-12-17 18:19:32 +0200

    main įdėtas backgroundas

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var GameState = {
     this.load.image('background', 'assets/images/background.png')
 },
 create: function(){
-
+this.background = this.game.add.sprite(0, 0, 'background');
 },
 update: function(){
 
